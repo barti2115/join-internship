cdfbb1eaba828f542e669050c5f5c4ed542c76ba
barti2115 <barti2115@icloud.com>
2022-04-09 22:21:17 +0200

    join class constructor

--- join.py
+++ join.py
@@ -1,17 +1,32 @@
 import csv
 
 
-class join:
-    def __init__(self, first_file, second_file, column_name, join_type):
-        with open(first_file, mode='r') as left_csv_file:
-        self.first_file = csv.DictReader(left_csv_file, delimiter=',')
-        with open(second_file, mode='r') as right_csv_file:
-        self.second_file = csv.DictReader(right_csv_file, delimiter=',')
+def open_file(file_path):
+    with open(file_path, mode='r') as left_csv_file:
+        return csv.DictReader(left_csv_file, delimiter=',')
+
+#TODO: rewrite Join to enable file to be open to prevent the i/o operation error
+class Join:
+    def __init__(self, first_file, second_file, column_name, join_type='inner'):
+        self.first_file = open_file(first_file)
+        self.second_file = open_file(second_file)
         self.column_to_join_on = column_name
         self.join_type = join_type
         self.first_file_cols = self.first_file.fieldnames
         self.second_file_cols = self.second_file.fieldnames
 
-    def open_file(self, file_path):
-        with open(file_path, mode='r') as left_csv_file:
-            return csv.DictReader(left_csv_file, delimiter=',')
+    def inner_join(self):
+        new_csv_body = ""
+        for first_file_row in self.first_file:
+            for second_file_row in self.second_file:
+                if first_file_row[self.column_to_join_on] == second_file_row[self.column_to_join_on]:
+                    for column in self.first_file_cols:
+                        new_csv_body += f"{second_file_row[column]},"
+                    for column in self.second_file_cols:
+                        if column != self.column_to_join_on:
+                            new_csv_body += f"{first_file_row[column]}"
+                            if column != self.second_file_cols[-1]:
+                                new_csv_body += ','
+                            else:
+                                new_csv_body += '\n'
+        return new_csv_body
--- main.py
+++ main.py
@@ -1,32 +1,6 @@
 import sys
-import csv
+import join
 
-column_name = sys.argv[3]
-new_csv_body = ""
-with open(sys.argv[1], mode='r') as left_csv_file:
-    left_csv_reader = csv.DictReader(left_csv_file, delimiter=',')
-    left_file_col_names = left_csv_reader.fieldnames
-    for left_row in left_csv_reader:
-        with open(sys.argv[2], mode='r') as right_csv_file:
-            right_csv_reader = csv.DictReader(right_csv_file, delimiter=',')
-            right_file_col_names = right_csv_reader.fieldnames
-            for right_row in right_csv_reader:
-                if left_row[column_name] == right_row[column_name]:
-                    for column in right_file_col_names:
-                        new_csv_body += f"{right_row[column]},"
-                    for column in left_file_col_names:
-                        if column != column_name:
-                            new_csv_body += f"{left_row[column]}"
-                            if column != left_file_col_names[-1]:
-                                new_csv_body += ','
-                            else:
-                                new_csv_body += '\n'
-    new_csv_header = ""
-    for column in right_file_col_names:
-        new_csv_header += f"{column},"
-    for column in left_file_col_names:
-        if column != column_name:
-            new_csv_header += f"{column}"
-            if column != left_file_col_names[-1]:
-                new_csv_header += ','
-    print(new_csv_header + '\n' + new_csv_body)
+#
+join = join.Join(sys.argv[1], sys.argv[2], sys.argv[3])
+print(join.inner_join())
