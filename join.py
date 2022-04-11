f1fcd6efdcb5fffe51204e3649b17db24270f611
barti2115 <barti2115@icloud.com>
2022-04-11 18:18:41 +0200

    parameters validation completed

--- join.py
+++ join.py
@@ -1,105 +1,137 @@
 import csv
 import sys
-#TODO CHECK IF FILE IS EMPTY OR IF FILE IS NOT CSV
+
 
 def generate_header(first_csv: str, second_csv: str, column_name: str):
     with open(first_csv, 'r') as first_file:
         with open(second_csv, 'r') as second_file:
             first_file_cols = list(csv.DictReader(first_file).fieldnames)
             second_file_cols = list(csv.DictReader(second_file).fieldnames)
-            try:
-                second_file_cols.pop(
-                    second_file_cols.index(column_name))  # removing duplicate of column we join on from second csv file
-            except ValueError:
-                print('ERROR! Column given to join on does not exist', file=sys.stderr)
-                return None
-            header = ','.join(first_file_cols + second_file_cols) + '\n'
-            return header
+            second_file_cols.pop(
+                second_file_cols.index(column_name))  # removing duplicate of column we join on from second csv file
+            header = ','.join(first_file_cols + second_file_cols)
+    print(header)
 
 
 def inner_join(first_csv: str, second_csv: str, column_name: str):
-    header = generate_header(first_csv, second_csv, column_name)
-    if header is not None:
-        with open(first_csv, 'r') as first_file:
-            new_csv_body = ""
-            first_iterator = csv.DictReader(first_file)
-            for first_file_row in first_iterator:
-                with open(second_csv, 'r') as second_file:
-                    second_iterator = csv.DictReader(second_file)
-                    for second_file_row in second_iterator:
-                        if first_file_row[column_name] == second_file_row[column_name]:
-                            new_csv_body += ','.join(first_file_row.values()) + ','
-                            second_file_row.pop(column_name)
-                            new_csv_body += ','.join(second_file_row.values()) + '\n'
-        return header + new_csv_body
+    with open(first_csv, 'r') as first_file:
+        first_iterator = csv.DictReader(first_file)
+        for first_file_row in first_iterator:
+            with open(second_csv, 'r') as second_file:
+                second_iterator = csv.DictReader(second_file)
+                for second_file_row in second_iterator:
+                    new_csv_chunk = ""
+                    if first_file_row[column_name] == second_file_row[column_name]:
+                        new_csv_chunk += ','.join(first_file_row.values()) + ','
+                        second_file_row.pop(column_name)
+                        new_csv_chunk += ','.join(second_file_row.values())
+                        print(new_csv_chunk)
 
 
 def fill_missing(left_file_row, right_file_row, join_type, column_name):
     if join_type == 'left':
-        return ','.join(left_file_row.values()) + ',' * (len(right_file_row)) + '\n'
+        print(','.join(left_file_row.values()) + ',' * (len(right_file_row) - 1))
     elif join_type == 'right':
         temp = ""
         for key in left_file_row:
             if key == column_name:
                 temp += right_file_row.pop(column_name)
             temp += ','
-        return temp + ','.join(right_file_row.values()) + '\n'
+        print(temp + ','.join(right_file_row.values()))
 
 
 def left_join(left_csv: str, right_csv: str, column_name: str):
-    header = generate_header(left_csv, right_csv, column_name)
-    if header is not None:
-        with open(left_csv, 'r') as left_file:
-            new_csv_body = ""
-            left_iterator = csv.DictReader(left_file)
-            for left_file_row in left_iterator:
-                new_csv_chunk = ""
-                with open(right_csv, 'r') as right_file:
-                    right_iterator = csv.DictReader(right_file)
-                    for right_file_row in right_iterator:
-                        if left_file_row[column_name] == right_file_row[column_name]:
-                            new_csv_chunk += ','.join(left_file_row.values()) + ','
-                            right_file_row.pop(column_name)
-                            new_csv_chunk += ','.join(right_file_row.values()) + '\n'
-                    if new_csv_chunk == "":
-                        new_csv_chunk += fill_missing(left_file_row, right_file_row, 'left', column_name)
-                new_csv_body += new_csv_chunk
-        return header + new_csv_body
+    with open(left_csv, 'r') as left_file:
+        left_iterator = csv.DictReader(left_file)
+        for left_file_row in left_iterator:
+            matched = False
+            with open(right_csv, 'r') as right_file:
+                right_iterator = csv.DictReader(right_file)
+                for right_file_row in right_iterator:
+                    new_csv_chunk = ""
+                    if left_file_row[column_name] == right_file_row[column_name]:
+                        new_csv_chunk += ','.join(left_file_row.values()) + ','
+                        right_file_row.pop(column_name)
+                        new_csv_chunk += ','.join(right_file_row.values())
+                        print(new_csv_chunk)
+                        matched = True
+                if not matched:
+                    fill_missing(left_file_row, right_file_row, 'left', column_name)
 
 
 def right_join(left_csv: str, right_csv: str, column_name: str):
-    header = generate_header(left_csv, right_csv, column_name)
-    if header is not None:
-        with open(right_csv, 'r') as right_file:
-            new_csv_body = ""
-            right_iterator = csv.DictReader(right_file)
-            for right_file_row in right_iterator:
-                new_csv_chunk = ""
-                with open(left_csv, 'r') as left_file:
-                    left_iterator = csv.DictReader(left_file)
-                    for left_file_row in left_iterator:
-                        if right_file_row[column_name] == left_file_row[column_name]:
-                            new_csv_chunk += ','.join(left_file_row.values()) + ','
-                            left_file_row.pop(column_name)
-                            new_csv_chunk += ','.join(left_file_row.values()) + '\n'
-                    if new_csv_chunk == "":
-                        new_csv_chunk += fill_missing(left_file_row, right_file_row, 'right', column_name)
-                new_csv_body += new_csv_chunk
-        return header + new_csv_body
+    with open(right_csv, 'r') as right_file:
+        right_iterator = csv.DictReader(right_file)
+        for right_file_row in right_iterator:
+            count = 0
+            with open(left_csv, 'r') as left_file:
+                left_iterator = csv.DictReader(left_file)
+                for left_file_row in left_iterator:
+                    new_csv_chunk = ""
+                    if right_file_row[column_name] == left_file_row[column_name]:
+                        new_csv_chunk += ','.join(left_file_row.values()) + ','
+                        left_file_row.pop(column_name)
+                        new_csv_chunk += ','.join(left_file_row.values())
+                        print(new_csv_chunk)
+                        count = 1
+                if count == 0:
+                    fill_missing(left_file_row, right_file_row, 'right', column_name)
 
 
-def join(first_csv: str, second_csv: str, column_name: str, join_type='inner'):
-    if join_type == 'inner':
-        new_csv = inner_join(first_csv, second_csv, column_name)
-        if new_csv is not None:
-            print(new_csv)
-    elif join_type == 'left':
-        new_csv = left_join(first_csv, second_csv, column_name)
-        if new_csv is not None:
-            print(new_csv)
-    elif join_type == 'right':
-        new_csv = right_join(first_csv, second_csv, column_name)
-        if new_csv is not None:
-            print(new_csv)
-    else:
-        print('ERROR! Wrong join type was given. Possible types are inner/left/right.', file=sys.stderr)
+def column_exist(file_header, column_name):
+    if column_name not in file_header:
+        return False
+    return True
+
+
+def valid_csv(file_name, column_name):
+    try:
+        with open(file_name, 'r') as first_file:
+            file_iterator = csv.DictReader(first_file)
+            if not column_exist(file_iterator.fieldnames, column_name):
+                print(f'ERROR! Column given to join on does not exist in {file_name}', file=sys.stderr)
+                return False
+            col_number = len(file_iterator.fieldnames)
+            for row in file_iterator:
+                if col_number - 1 != str(row.values()).count(','):
+                    print(f'ERROR! {file_name} is not a valid csv.', file=sys.stderr)
+                    return False
+    except FileNotFoundError:
+        print('ERROR! Wrong filepath was given.', file=sys.stderr)
+        return False
+    return True
+
+
+def files_fine(first_file, second_file, column_name):
+    if not valid_csv(first_file, column_name) or not valid_csv(second_file, column_name):
+        return False
+    return True
+
+
+def validate_arguments(argv):
+    if len(argv) < 4:
+        print('ERROR! Not enough parameters.', file=sys.stderr)
+        return False
+    elif not files_fine(argv[1], argv[2], argv[3]):
+        return False
+    return True
+
+
+def join(argv):
+    if validate_arguments(argv):
+        first_csv = argv[1]
+        second_csv = argv[2]
+        column_name = argv[3]
+        if len(argv) == 4:
+            join_type = 'inner'
+        else:
+            join_type = argv[4]
+        generate_header(first_csv, second_csv, column_name)
+        if join_type == 'inner':
+            inner_join(first_csv, second_csv, column_name)
+        elif join_type == 'left':
+            left_join(first_csv, second_csv, column_name)
+        elif join_type == 'right':
+            right_join(first_csv, second_csv, column_name)
+        else:
+            print('ERROR! Wrong join type was given. Possible types are inner/left/right.', file=sys.stderr)
--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import sys
 import join
 
-try:
-    join.join(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
-except FileNotFoundError:
-    print('ERROR! Wrong filepath was given.', file=sys.stderr)
-except IndexError:
-    try:
-        join.join(sys.argv[1], sys.argv[2], sys.argv[3])
-    except IndexError:
-        print('ERROR! Not enough parameters.', file=sys.stderr)
-    except FileNotFoundError:
-        print('ERROR! Wrong filepath was given.', file=sys.stderr)
+join.join(sys.argv)
